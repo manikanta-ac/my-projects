cb947d3135d5a4caa9b79955cfa8869fcd1c77fa
Manikanta A C <ac.manikanta506@gmail.com>
2017-07-26 17:01:01 +0530

    Added camel-beanio project components

---
+++ camel-beanio sample/MessageConverter.java
@@ -0,0 +1,22 @@
+package com.evry.proj.message;
+
+import java.util.Map;
+import org.apache.camel.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class MessageConverter {
+    private static final String PAYLOAD = "message.payload";
+
+    @Converter
+    public static String convertToInwardMessage(Object messageDetail) {
+
+        @SuppressWarnings("unchecked")
+        Map<String, Object> messageDetailMap = (Map<String, Object>) messageDetail;
+        String payload = (String) messageDetailMap.get(PAYLOAD);
+		/*
+			Business Logic
+		*/
+        return payload;
+    }
+}
